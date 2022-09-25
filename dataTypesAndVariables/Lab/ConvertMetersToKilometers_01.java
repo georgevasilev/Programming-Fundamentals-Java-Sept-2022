168d76bf039f56834a8b249c5ca0cc373198e458
George Vasilev <114221933+georgevasilev@users.noreply.github.com>
2022-09-25 13:15:11 +0300

    Data Types and Variables Lab
    Solutions for Data Types and Variables Lab problems

---
+++ dataTypesAndVariables/Lab/ConvertMetersToKilometers_01.java
@@ -0,0 +1,14 @@
+package dataTypesAndVariables.Lab;
+
+import java.util.Scanner;
+
+public class ConvertMetersToKilometers_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int meters = Integer.parseInt(scanner.nextLine());
+        double km = meters /1000.0;
+
+        System.out.printf("%.2f", km);
+    }
+}
